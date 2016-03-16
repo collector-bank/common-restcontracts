763bbedad2466f78dd2194aeb037e49372887bb1
Alexander Zachrisson <alexander.zachrisson@collector.se>
2016-03-16 13:33:19 +0100

    Saknad fil

---
+++ Collector.Common.RestContracts/ErrorInfo.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ErrorInfo.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Collector.Common.RestContracts
+{
+    /// <summary>
+    /// Additional error information.
+    /// </summary>
+    public class ErrorInfo
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorInfo"/> class.
+        /// </summary>
+        /// <param name="reason">The reason.</param>
+        /// <param name="message">The message.</param>
+        public ErrorInfo(string reason, string message)
+        {
+            Reason = reason;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Gets the error message.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Gets the error reason. 
+        /// </summary>
+        public string Reason { get; }
+    }
+}
