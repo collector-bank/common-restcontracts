df3f1c6702aa964d9f5ba4cd761eb3496c0f3875
Alexander Zachrisson <alexander.zachrisson@collector.se>
2016-03-16 13:24:15 +0100

    Fixat AssemblyInfo

--- Collector.Common.RestContracts/Properties/AssemblyInfo.cs
+++ Collector.Common.RestContracts/Properties/AssemblyInfo.cs
@@ -1,5 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
