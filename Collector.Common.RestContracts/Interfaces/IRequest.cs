4b13bf44ab7140423b7d46218d597b500e3232ca
Adam Lith <Adam.Lith@collectorbank.se>
2017-07-04 07:55:03 +0200

    Pulled upp some methods to the IRequest interface. CR: Martin #US25606

--- Collector.Common.RestContracts/Interfaces/IRequest.cs
+++ Collector.Common.RestContracts/Interfaces/IRequest.cs
@@ -6,10 +6,29 @@
 
 namespace Collector.Common.RestContracts.Interfaces
 {
+    using System.Collections.Generic;
+
     /// <summary>
     /// Marker interface for all requests
     /// </summary>
     public interface IRequest
-    {        
+    {
+        /// <summary>
+        /// Gets the Http method.
+        /// </summary>
+        /// <returns>The Http method.</returns>
+        HttpMethod GetHttpMethod();
+
+        /// <summary>
+        /// Gets the key for loading base URL, authentification logic, logging etc..
+        /// </summary>
+        /// <returns>The configuration key name.</returns>
+        string GetConfigurationKey();
+
+        /// <summary>
+        /// Validates the request.
+        /// </summary>
+        /// <returns>A list of validation error infos.</returns>
+        IEnumerable<ErrorInfo> GetValidationErrors();
     }
 }
