72bf2fe1d99c0a550109d636ac7787304e52b5d1
Adam Lith <adam.lith@gmail.com>
2017-07-25 09:22:48 +0200

    Adding parser interfaces for custom response parsing.

---
+++ src/Collector.Common.RestContracts/Interfaces/ISuccessfulResponseParser.cs
@@ -0,0 +1,10 @@
+﻿namespace Collector.Common.RestContracts.Interfaces
+{
+    /// <summary>
+    /// Implement this interface on your request class if you want to control how successful data objects are parsed from the raw response of the server.
+    /// </summary>
+    public interface ISuccessfulResponseParser<out TResponse>
+    {
+        TResponse ParseResponse(string content);
+    }
+}
