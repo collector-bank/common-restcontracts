0cc30493ff46ebee2693020777660bbda0061e48
Magnus Törnvall <magnus.tornvall@collectorbank.se>
2018-10-09 09:57:35 +0200

    Revert ISuccessfulResponseParser

--- src/Collector.Common.RestContracts/Interfaces/ISuccessfulResponseParser.cs
+++ src/Collector.Common.RestContracts/Interfaces/ISuccessfulResponseParser.cs
@@ -3,8 +3,8 @@
     /// <summary>
     /// Implement this interface on your request class if you want to control how successful data objects are parsed from the raw response of the server.
     /// </summary>
-    public interface ISuccessfulResponseParser
+    public interface ISuccessfulResponseParser<out TResponse>
     {
-        TResponse ParseResponse<TResponse>(string content);
+        TResponse ParseResponse(string content);
     }
 }
