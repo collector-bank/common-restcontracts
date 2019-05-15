9eca5203b194f2d80364bb82a97d422c1974c3b0
Magnus Törnvall <magnus.tornvall@collectorbank.se>
2019-05-15 16:37:54 +0200

    Fix broken test

--- test/Collector.Common.RestContracts.UnitTest/RequestBase_Test.cs
+++ test/Collector.Common.RestContracts.UnitTest/RequestBase_Test.cs
@@ -1,5 +1,7 @@
 ﻿namespace Collector.Common.RestContracts.UnitTest
 {
+    using System.Linq;
+
     using Collector.Common.RestContracts.Interfaces;
 
     using NUnit.Framework;
@@ -7,11 +9,21 @@
     public class RequestBase_Test
     {
         [Test]
-        public void When_creating_any_request_then_headers_is_not_null()
+        public void When_creating_any_request_then_get_headers_never_returns_null()
+        {
+            var sut = new MyRequest();
+            
+            Assert.NotNull(sut.GetHeaders());
+        }
+
+        [Test]
+        public void When_creating_any_request_adding_a_header_then_get_headers_contains_added_header()
         {
             var sut = new MyRequest();
 
-            Assert.NotNull(sut.Headers);
+            sut.AddHeader("test", "test");
+
+            Assert.NotNull(sut.GetHeaders().FirstOrDefault(h => h.Key == "test"));
         }
 
         private class MyRequest : RequestBase<MyResourceIdentifier>
